827c45aa605e4d3c31c1a23605b60dd3e7f39495
madhan-swe <madhankumarcvkl@gmail.com>
2021-02-16 21:38:21 +0530

    Assets added

---
+++ src/assets/icons.jsx
@@ -0,0 +1,40 @@
+import node from "./node.png"
+import css from "./css.png"
+import html from "./html.png"
+import javascript from "./javascript.png"
+import r from "./r.png"
+import docker from "./docker.png"
+import kubernetes from "./kubernetes.png"
+import jenkins from "./jenkins.png"
+import play from "./play.png"
+import code from "./code.png"
+import react from "./react.png"
+import typescript from "./typescript.png"
+import python from "./python.png"
+import mongo from "./mongodb.png"
+import ansible from "./ansible.png"
+import terraform from "./terraform.png"
+import express from "./express.png"
+import bootstrap from "./bootstrap.png"
+
+export const ICONS = {
+    "node": [node, "Node JS"],
+    "javascript": [javascript, "Javascript"],
+    "css": [css, "CSS"],
+    "html": [html, "HTML"],
+    "r": [r, "R"],
+    "jenkins": [jenkins, "Jenkins"],
+    "docker": [docker, "Docker"],
+    "kubernetes": [kubernetes, "Kubernetes"],
+    "python": [python, "Python"],
+    "demo": [play, "Demo"],
+    "code": [code, "Code"],
+    "react": [react, "React JS"],
+    "typescript": [typescript, "TypeScript"],
+    "mongo": [mongo, "Mongo DB"],
+    "ansible": [ansible, "Ansible"],
+    "terraform": [terraform, "Terraform"],
+    "express": [express, "Express JS"],
+    "bootstrap": [bootstrap, "Bootstrap"]
+
+}
