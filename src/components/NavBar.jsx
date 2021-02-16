9e067c9cbe4fb0addf4719fdfc35b384e790df58
madhan-swe <madhankumarcvkl@gmail.com>
2021-02-16 21:39:11 +0530

    Navbar added

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+           <div className="row">
+           <div className="col-12">
+            <ul className="app_navigation bg-light text-black">
+                <li className="app_navigation_item">
+                    Home
+                </li>
+                <li className="app_navigation_item">
+                    Education
+                </li>
+                <li className="app_navigation_item">
+                    Experience
+                </li>
+                <li className="app_navigation_item">
+                    Skills
+                </li>
+                <li className="app_navigation_item">
+                    Projects
+                </li>
+            </ul>
+            </div>
+            </div>
+    )
+}
