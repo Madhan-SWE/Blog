e0d0638c6a5c5149ee3b4294febd3707c4977092
madhan-swe <madhankumarcvkl@gmail.com>
2021-02-16 21:39:45 +0530

    Experience added

---
+++ src/components/Experience.jsx
@@ -0,0 +1,168 @@
+import React from "react";
+
+export default function Experience() {
+    return (
+        <div className="row app_experience_bar">
+            <div className="col-12">
+                <div className="row ">
+                    <div className="col-12">
+                        <div className="row">
+                            <div className="col-sm-12 mb-2 mt-2">
+                                <div class="card text-black bg-light">
+                                    <div class="card-header">
+                                        <div className="row">
+                                            <div className="col-6 text-left">
+                                                <h3 class="card-title">
+                                                    {" "}
+                                                    Maplelabs (Xoriant
+                                                    solutions)
+                                                </h3>
+                                                <h4> Software Engineer</h4>
+                                                <h6>
+                                                    <i class="fas fa-map-marker-alt pr-2"></i>
+                                                    Bangalore, India
+                                                </h6>
+                                            </div>
+                                            <div className="col-6 text-right">
+                                                <h5> July 2018 - Present</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <h2>Snappy Flow</h2>
+                                        <ul>
+                                            <li>
+                                                Developed python plugin to get
+                                                metrics of the oozie, yarn, name
+                                                node, and spark. Processing
+                                                these collected metrics and
+                                                storing results in Elastic
+                                                search
+                                            </li>
+                                            <li>
+                                                Automated running spark, hive,
+                                                and oozie jobs on Hadoop cluster
+                                                after it is deployed from
+                                                SnappyFlow
+                                            </li>
+                                            <li>
+                                                Converted Hadoop deployment
+                                                templates from(terraform) AWS
+                                                infrastructure to Vcenter
+                                                infrastructure
+                                            </li>
+                                        </ul>
+
+                                        <h2>
+                                            Hyperflex Appliance Platform (HxAP)
+                                            & Intersight
+                                        </h2>
+                                        <ul>
+                                            <li>
+                                                Working on the development of
+                                                automation framework and added
+                                                libraries for verifying the
+                                                various features of HXAP and
+                                                Intersight
+                                            </li>
+                                            <li>
+                                                Created various Jenkins
+                                                pipelined jobs for CICD of HXAP
+                                                platform
+                                            </li>
+                                            <li>
+                                                Planned various UI and API test
+                                                plans for HXAP and Intersight
+                                            </li>
+                                            <li>
+                                                Automated various Operations in
+                                                HXAP deployment and
+                                                configuration
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-12 mb-2 mt-2">
+                                <div class="card text-black bg-light">
+                                    <div class="card-header">
+                                        <div className="row">
+                                            <div className="col-6 text-left">
+                                                <h3 class="card-title">
+                                                    {" "}
+                                                    Maplelabs (Xoriant
+                                                    solutions)
+                                                </h3>
+                                                <h4> Software Engineer</h4>
+                                                <h6>
+                                                    <i class="fas fa-map-marker-alt pr-2"></i>
+                                                    Bangalore, India
+                                                </h6>
+                                            </div>
+                                            <div className="col-6 text-right">
+                                                <h5> July 2018 - Present</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <h2>Snappy Flow</h2>
+                                        <ul>
+                                            <li>
+                                                Developed python plugin to get
+                                                metrics of the oozie, yarn, name
+                                                node, and spark. Processing
+                                                these collected metrics and
+                                                storing results in Elastic
+                                                search
+                                            </li>
+                                            <li>
+                                                Automated running spark, hive,
+                                                and oozie jobs on Hadoop cluster
+                                                after it is deployed from
+                                                SnappyFlow
+                                            </li>
+                                            <li>
+                                                Converted Hadoop deployment
+                                                templates from(terraform) AWS
+                                                infrastructure to Vcenter
+                                                infrastructure
+                                            </li>
+                                        </ul>
+
+                                        <h2>
+                                            Hyperflex Appliance Platform (HxAP)
+                                            & Intersight
+                                        </h2>
+                                        <ul>
+                                            <li>
+                                                Working on the development of
+                                                automation framework and added
+                                                libraries for verifying the
+                                                various features of HXAP and
+                                                Intersight
+                                            </li>
+                                            <li>
+                                                Created various Jenkins
+                                                pipelined jobs for CICD of HXAP
+                                                platform
+                                            </li>
+                                            <li>
+                                                Planned various UI and API test
+                                                plans for HXAP and Intersight
+                                            </li>
+                                            <li>
+                                                Automated various Operations in
+                                                HXAP deployment and
+                                                configuration
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
