f4fde5b73bc1a5c950d4b02cf68abb9a356cea09
madhan-swe <madhankumarcvkl@gmail.com>
2021-02-17 20:40:52 +0530

    App files modified

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
+
 import SideNavBar from "./components/SideNavBar";
 import ContentBar from "./components/ContentBar";
 
-
 function App() {
-  return(
-    <>
-    <div className="row app_bar p-3">
-        <SideNavBar />
-        <ContentBar />
+    const [activeItem, setActiveItem] = useState("Home");
 
-    </div>
-    </>
-  )
+    function changeActiveItem(value) {
+        setActiveItem(value);
+        console.log(value)
+    }
+    return (
+        <>
+            <div className="row app_bar p-3">
+                <SideNavBar />
+                <ContentBar
+                    activeItem={activeItem}
+                    changeActiveItem={changeActiveItem}
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
