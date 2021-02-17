9b87a746aa3a4148e5729f224e93b58d00e58015
madhan-swe <madhankumarcvkl@gmail.com>
2021-02-17 20:40:29 +0530

    Store modified

--- src/store/constants.jsx
+++ src/store/constants.jsx
@@ -4,41 +4,40 @@ export const PROJECTS = [
         "code": "https://github.com/Madhan-SWE/ZEN-class-tasks/tree/master/Day17-120920/QuizApp",
         "demo": "https://zen-fermat-35f995.netlify.app",
         "description": "Quiz game front end application developed with OPEN Trivia DB APIs",
-        "languages": ["html", "css", "typescript"]
+        "languages": ["html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript"],
+        "categories": ["All"]
     },
     {
         "name": "QUIZ APPLICATION",
         "code": "https://github.com/Madhan-SWE/ZEN-class-tasks/tree/master/Day17-120920/QuizApp",
         "demo": "https://zen-fermat-35f995.netlify.app",
         "description": "Quiz game front end application developed with OPEN Trivia DB APIs",
-        "languages": ["html", "css", "typescript"]
+        "languages": ["html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript"],
+        "categories": ["All"]
     },
     {
         "name": "QUIZ APPLICATION",
         "code": "https://github.com/Madhan-SWE/ZEN-class-tasks/tree/master/Day17-120920/QuizApp",
         "demo": "https://zen-fermat-35f995.netlify.app",
         "description": "Quiz game front end application developed with OPEN Trivia DB APIs",
-        "languages": ["html", "css", "typescript", "node"]
-    },
-    {
-        "name": "QUIZ APPLICATIONQUIZ APPLICATIONQUIZ APPLICATIONQUIZ APPLICATIONQUIZ APPLICATION",
-        "code": "https://github.com/Madhan-SWE/ZEN-class-tasks/tree/master/Day17-120920/QuizApp",
-        "demo": "https://zen-fermat-35f995.netlify.app",
-        "description": "Quiz game front end application developed with OPEN Trivia DB APIs",
-        "languages": ["html", "css", "typescript"]
-    },
-    {
-        "name": "QUIZ APPLICATION",
-        "code": "https://github.com/Madhan-SWE/ZEN-class-tasks/tree/master/Day17-120920/QuizApp",
-        "demo": "https://zen-fermat-35f995.netlify.app",
-        "description": "Quiz game front end application developed with OPEN Trivia DB APIs Quiz game front end application developed with OPEN Trivia DB API",
-        "languages": ["html", "css", "typescript"]
+        "languages": ["html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript"],
+        "categories": ["All"]
     },
     {
         "name": "QUIZ APPLICATION",
         "code": "https://github.com/Madhan-SWE/ZEN-class-tasks/tree/master/Day17-120920/QuizApp",
         "demo": "https://zen-fermat-35f995.netlify.app",
         "description": "Quiz game front end application developed with OPEN Trivia DB APIs",
-        "languages": ["html", "css", "typescript"]
+        "languages": ["html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript"],
+        "categories": ["All"]
+    },
+    {
+        "name": "QUIZ APPLICATION",
+        "code": "https://github.com/Madhan-SWE/ZEN-class-tasks/tree/master/Day17-120920/QuizApp",
+        "demo": "https://zen-fermat-35f995.netlify.app",
+        "description": "Quiz game front end application developed with OPEN Trivia DB APIs",
+        "languages": ["html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript", "html", "css", "typescript"],
+        "categories": ["All"]
     }
+
 ]
